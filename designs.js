2405912cf5b5471c196a240005a089aa21cca4b4
TosinAdedeji <adesewatosin@gmail.com>
2018-06-30 14:24:57 -0700

    final changes

--- designs.js
+++ designs.js
@@ -8,28 +8,27 @@ $(function(){
 	//code goes here
 
 	function makeGrid(height, width) {
-
-       const tableRowBeginTag = "<tr>";
-       const tableRowCloseTag = "</tr>";
-       const tableDataTag = "<td></td>";
-       //clears grid whenever makegrid() is called
-       let insideTable = "";
+        //create inside table tags
+       const tRowBegin = "<tr>";
+       const tRowClose = "</tr>";
+       const tableData = "<td></td>";
+       //
+       let table = "";
        let i = 0;
        
        while (i < height) {
-           insideTable += tableRowBeginTag;
+           table += tRowBegin;
            for (let x = 0; x < width; x++) {
-               insideTable += tableDataTag;
+               table += tableData;
            }
-           insideTable += tableRowCloseTag;
+           table += tRowClose;
            i++;
        }
-       $("#pixelCanvas").html(insideTable);
+       $("#pixelCanvas").html(table);
 
    }
 
-   //prevent reload on submit and get height & width value chosen by user,
-   //convert to number type and call the makegrid()
+   //
    $("#sizePicker").submit(function (event) {
        event.preventDefault();
        let width = Number($("#inputWidth").val());
@@ -37,8 +36,7 @@ $(function(){
        makeGrid(height, width);
    });
 
-   //click event that uses jquery delegation to get color value
-   // and color grid clicked by user
+   //attach an event to all td's
    $("table").on("click", "td", function () {
        let color = $("#colorPicker").val();
        $(this).css("background", color);
